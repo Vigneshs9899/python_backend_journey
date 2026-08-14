44bff80c3bf4b65b85efe0dd9e9ff0bd8e99ff40
Vigneshs9899 <vigneshs9899@gmail.com>
2026-08-14 16:19:32 +0530

    Learn Python packages and imports

---
+++ 01_Python/package_test.py
@@ -0,0 +1,10 @@
+from calculator import add,multiply, subtract
+
+result_add = add(10,20)
+print(result_add)
+
+result_multiply = multiply(10,20)
+print(result_multiply)
+
+result_subtract = subtract(10,20)
+print(result_subtract)
