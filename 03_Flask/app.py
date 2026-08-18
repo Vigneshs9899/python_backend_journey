d48b354915ec7d651bfa27b126926c0a47690ddd
Vigneshs9899 <vigneshs9899@gmail.com>
2026-08-18 18:56:13 +0530

    Add employee API validation

--- 03_Flask/app.py
+++ 03_Flask/app.py
@@ -21,11 +21,39 @@ def api():
 @app.route("/employees", methods=["POST"])
 def add_employee():
     employee = request.get_json()
-    return {
-        "message": "Employee recieved successfully",
-        "employee": employee
-    }
 
+    required_fields = ["id", "name", "salary", "department"]
+    for fields in required_fields:
+       
+        if fields not in employee:
+           return{
+                       "message": f"Employee {fields} is required"
+                   }, 400
+        
+        if not isinstance(employee["id"], int):
+                    return{
+                        "message": "ID must be an integer"
+                    }, 400
+        
+        if not isinstance(employee["name"], str):
+                    return{
+                        "message": "Name must be string"
+                    }, 400
+
+        if not isinstance(employee["department"], str):
+                    return{
+                        "message": "Department must be string"
+                    }, 400
+        
+        if not isinstance(employee["salary"], int):
+            return{
+                "message": "Salary must be an integer"
+            }, 400
+        
+    return {
+               "message": "Employee recieved successfully",
+               "employee": employee
+           }, 200
 
 if(__name__) == "__main__":
     app.run(debug=True)
